e5709e7180b10da9e2f52145a54925a4191a64b6
yadurajgupta <yaduraj1997@gmail.com>
2021-06-10 18:43:38 +0530

    commit

--- Sorting and Searching/Josephus_Problem_II.cpp
+++ Sorting and Searching/Josephus_Problem_II.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
 
 #define IOS                  \
     ios::sync_with_stdio(0); \
     cin.tie(0);              \
     cout.tie(0);
-#undef INT_MAX
-#define INT_MAX LLONG_MAX
 #define endl '\n'
 #define int long long
+#define ordered_set(type) __gnu_pbds::tree<type, __gnu_pbds::null_type, less<type>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>
 #define forab(ii, aa, bb) for (int ii = aa; ii < bb; ii++)
 #define forabd(ii, aa, bb) for (int ii = aa; ii >= bb; ii--)
 #define forabi(ii, aa, bb, inc) for (int ii = aa; ii < bb; ii += inc)
+#define forabdi(ii, aa, bb, inc) for (int ii = aa; ii >= bb; ii -= inc)
 #define all(aa) aa.begin(), aa.end()
 #define rall(aa) aa.rbegin(), aa.rend()
-#define twodi(type, name, NN, MM, II) vector<vector<type>> name(NN, vector<type>(MM, II));
-#define twod(type, name, NN, MM) vector<vector<type>> name(NN, vector<type>(MM));
-#define threedi(type, name, NN, MM, LL, II) vector<vector<vector<type>>> name(NN, vector<vector<type>>(MM, vector<type>(LL, II)));
-#define threed(type, name, NN, MM, LL) vector<vector<vector<type>>> name(NN, vector<vector<type>>(MM, vector<type>(LL)));
+#define twodi(type, name, NN, MM, II) vector<vector<type>> name(NN, vector<type>(MM, II))
+#define twod(type, name, NN, MM) vector<vector<type>> name(NN, vector<type>(MM))
+#define threedi(type, name, NN, MM, LL, II) vector<vector<vector<type>>> name(NN, vector<vector<type>>(MM, vector<type>(LL, II)))
+#define threed(type, name, NN, MM, LL) vector<vector<vector<type>>> name(NN, vector<vector<type>>(MM, vector<type>(LL)))
 #define PL cout << endl
-#define PS(aa) cout << #aa << ' ' << aa << ' ';
+#define PS(aa) cout << #aa << ' ' << aa << ' '
 #define PSL(aa)               \
     cout << #aa << ' ' << aa; \
     PL;
@@ -27,6 +30,14 @@
     for (auto &ii : vv)    \
         cout << ii << ' '; \
     PL;
+#define print2d(vv)            \
+    for (auto &row : vv)       \
+    {                          \
+        for (auto &ii : row)   \
+            cout << ii << ' '; \
+        cout << endl;          \
+    }
+#define printp(vv) cout << "{" << vv.firt << "," << vv.second << "} "
 #define printn(vv, NN)                      \
     forab(ii, 0, NN) cout << vv[ii] << ' '; \
     PL;
@@ -34,58 +45,54 @@
     vector<type> vv(NN);    \
     for (auto &ii : vv)     \
         cin >> ii;
-
-using namespace std;
+void debug()
+{
+    PL;
+}
+template <typename T, typename... Types>
+void debug(T var1, Types... var2)
+{
+    cout << var1 << " ";
+    debug(var2...);
+}
+/* TEMPLATE END */
 int N, K;
-struct CircularQueue
+struct CircuclarQueue
 {
     int data;
-    CircularQueue *next, *prev;
+    CircuclarQueue *prev = NULL;
+    CircuclarQueue *next = NULL;
+    CircuclarQueue(int num)
+    {
+        data = num;
+    }
 };
-CircularQueue *giveNode(int num)
-{
-    CircularQueue *node = new CircularQueue();
-    node->data = num;
-    node->next = NULL;
-    node->prev = NULL;
-    return node;
-}
+const int NMAX = 2 * (1e5) + 10;
+const int LGMAX = 20;
+array<array<CircuclarQueue *, LGMAX>, NMAX> dp;
 void solve()
 {
     cin >> N >> K;
-    // K++;
+    // N = 7;
+    // K = 2;
+
     if (N == 1)
     {
         cout << 1 << endl;
         return;
     }
-    vector<CircularQueue *> vec;
+    ordered_set(int) st;
     forab(i, 1, N + 1)
+        st.insert(i);
+    int k = K % N;
+    while (N)
     {
-        vec.push_back(giveNode(i));
-    }
-    forab(i, 0, N)
-    {
-        vec[i]->next = vec[(i + 1 + N) % N];
-        vec[i]->prev = vec[(i - 1 + N) % N];
-    }
-    int sz = N;
-    int k = (K) % N;
-    CircularQueue *node = vec[k];
-    CircularQueue *prev;
-    CircularQueue *next;
-    while (N--)
-    {
-        cout << node->data << " ";
-        prev = node->prev;
-        next = node->next;
-        prev->next = next;
-        next->prev = prev;
-        node = next;
-        while (k--)
-            node = node->next;
+        auto it = st.find_by_order(k);
+        cout << *it << " ";
+        st.erase(it);
+        N--;
         if (N)
-            k = (K) % N;
+            k = (k % N + K) % N;
     }
 }
 int32_t main()
