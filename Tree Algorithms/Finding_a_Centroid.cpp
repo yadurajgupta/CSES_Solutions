9f487d05c093114085476263449fb637b2a6ffd4
yadurajgupta <yaduraj1997@gmail.com>
2021-06-19 23:42:26 +0530

    Commit

---
+++ Tree Algorithms/Finding_a_Centroid.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+
+#define IOS                  \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define endl '\n'
+#define int long long
+#define ordered_set(type) __gnu_pbds::tree<type, __gnu_pbds::null_type, less<type>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>
+#define forab(ii, aa, bb) for (int ii = aa; ii < bb; ii++)
+#define forabd(ii, aa, bb) for (int ii = aa; ii >= bb; ii--)
+#define forabi(ii, aa, bb, inc) for (int ii = aa; ii < bb; ii += inc)
+#define forabdi(ii, aa, bb, inc) for (int ii = aa; ii >= bb; ii -= inc)
+#define all(aa) aa.begin(), aa.end()
+#define rall(aa) aa.rbegin(), aa.rend()
+#define twodi(type, name, NN, MM, II) vector<vector<type>> name(NN, vector<type>(MM, II))
+#define twod(type, name, NN, MM) vector<vector<type>> name(NN, vector<type>(MM))
+#define threedi(type, name, NN, MM, LL, II) vector<vector<vector<type>>> name(NN, vector<vector<type>>(MM, vector<type>(LL, II)))
+#define threed(type, name, NN, MM, LL) vector<vector<vector<type>>> name(NN, vector<vector<type>>(MM, vector<type>(LL)))
+#define PL cout << endl
+#define PS(aa) cout << #aa << ' ' << aa << ' '
+#define PSL(aa)               \
+    cout << #aa << ' ' << aa; \
+    PL;
+#define print(vv)          \
+    for (auto &ii : vv)    \
+        cout << ii << ' '; \
+    PL;
+#define print2d(vv)            \
+    for (auto &row : vv)       \
+    {                          \
+        for (auto &ii : row)   \
+            cout << ii << ' '; \
+        cout << endl;          \
+    }
+#define printp(vv) cout << '{' << vv.firt << ' ' << vv.second << '}' << ' '
+#define printn(vv, NN)                      \
+    forab(ii, 0, NN) cout << vv[ii] << ' '; \
+    PL;
+#define invec(type, vv, NN) \
+    vector<type> vv(NN);    \
+    for (auto &ii : vv)     \
+        cin >> ii;
+void debug()
+{
+    PL;
+}
+template <typename T, typename... Types>
+void debug(T var1, Types... var2)
+{
+    cout << var1 << ' ';
+    debug(var2...);
+}
+/* TEMPLATE END */
+int N, M, x, y;
+const int NMAX = 2e5 + 5;
+array<vector<int>, NMAX> adj;
+array<int, NMAX> subtree_elems;
+int dfs1(int node, int par)
+{
+    int elems = 1;
+    for (int next : adj[node])
+    {
+        if (next == par)
+            continue;
+        elems += dfs1(next, node);
+    }
+    return subtree_elems[node] = elems;
+}
+int find_centroid(int node, int par)
+{
+    // debug(node, par);
+    int child_with_mx_elems = -1;
+    int mx_child_elems = -1;
+    for (int next : adj[node])
+    {
+        if (next == par)
+            continue;
+        if (mx_child_elems < subtree_elems[next])
+            mx_child_elems = subtree_elems[next], child_with_mx_elems = next;
+    }
+    // debug(child_with_mx_elems, mx_child_elems);
+    if (mx_child_elems <= N / 2)
+        return node;
+    else
+        return find_centroid(child_with_mx_elems, node);
+}
+void solve()
+{
+    cin >> N;
+    M = N - 1;
+    forab(i, 0, M)
+    {
+        cin >> x >> y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+    dfs1(1, 0);
+    cout << find_centroid(1, 0) << endl;
+}
+int32_t main()
+{
+    IOS;
+    solve();
+    return 0;
+}
