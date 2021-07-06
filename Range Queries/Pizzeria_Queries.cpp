dce006f7e641dc68e7803481cdb9f88ef3341ae8
yaduraj1997 <yaduraj1997@gmail.com>
2021-07-06 22:08:05 +0530

    Commit

--- Range Queries/Pizzeria_Queries.cpp
+++ Range Queries/Pizzeria_Queries.cpp
@@ -1,3 +1,5 @@
+// replace slashes then double qutotes then stringify
+
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
@@ -5,6 +7,7 @@
 using namespace std;
 
 // #define _DEBUG_CODE 1
+// #define ONLINE_JUDGE 1
 #pragma region
 // clang-format off
 #define int long long
@@ -20,6 +23,7 @@ using namespace std;
 #define mkp make_pair
 #define mka(type, SZ) array<type, SZ>
 #define printn(vv, NN) forab(ii, 0, NN) _debug_p(vv[ii]), cout << ' '; PL;
+
 #ifndef ONLINE_JUDGE
     #define PNAV(x) cout << #x << ' '; _debug_p(x); cout << ' ';
     #define debug(x1) PNAV(x1) cout << endl;
@@ -28,158 +32,199 @@ using namespace std;
     #define debug4(x1, x2, x3, x4) PNAV(x1); debug3(x2, x3, x4);
     #define debug5(x1, x2, x3, x4, x5) PNAV(x1); debug4(x2, x3, x4, x5);
     #define debug6(x1, x2, x3, x4, x5, x6) PNAV(x1); debug5(x2, x3, x4, x5, x6);
+    #define debug7(x1, x2, x3, x4, x5, x6, x7) PNAV(x1); debug6(x2, x3, x4, x5, x6, x7);
+    #define debug8(x1, x2, x3, x4, x5, x6, x7, x8) PNAV(x1); debug7(x2, x3, x4, x5, x6, x7, x8);
+    #define debug9(x1, x2, x3, x4, x5, x6, x7, x8, x9) PNAV(x1); debug8(x2, x3, x4, x5, x6, x7, x8, x9);
+    #define debug10(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10) PNAV(x1); debug9(x2, x3, x4, x5, x6, x7, x8, x9, x10);
+    #define debugab(x1, s, e) cout << #x1 << '[' << min((int)s, (int)e) << ':' << max((int)s, (int)e) << ']' << ' '; _debug_p_vector_typeab(x1, min((int)s, (int)e), max((int)s, (int)e) + 1); cout<<endl;
+    void _debug_p(int T);
+    void _debug_p(int32_t T);
+    void _debug_p(unsigned int T);
+    void _debug_p(unsigned T);
+    void _debug_p(double T);
+    void _debug_p(string T);
+    void _debug_p(const char T[]);
+    void _debug_p(char T);
+    void _debug_p(bool T);
+    template <typename T> void _debug_p(const vector<T> &vec);
+    template <typename T> void _debug_p(const list<T> &vec);
+    template <typename T,size_t sz> void _debug_p(const array<T,sz> &vec);
+    template <typename T> void _debug_p(const set<T> &vec);
+    template <typename T> void _debug_p(const unordered_set<T> &vec);
+    template <typename T> void _debug_p(const multiset<T> &vec);
+    template <typename T> void _debug_p(const unordered_multiset<T> &vec);
+    template <typename t1, typename t2> void _debug_p(const map<t1, t2> &mp);
+    template <typename t1, typename t2> void _debug_p(const unordered_map<t1, t2> &mp);
+    template <typename t1, typename t2> void _debug_p(const multimap<t1, t2> &mp);
+    template <typename t1, typename t2> void _debug_p(const unordered_multimap<t1, t2> &mp);
+    template <class... T> void _debug_p(const tuple<T...> &_tup);
+    template <class TupType, size_t... I> void _debug_p_tup(const TupType &_tup, index_sequence<I...>);
+    template <typename T> void _debug_p_vector_type(const T &t);
+    template <typename T> void _debug_p_vector_typeab(const T &t,int s,int e);
+    template <typename T> void _debug_p_map_type(const T &t);
+    template <typename t1, typename t2> void _debug_p(const pair<t1, t2> &p);
 #endif
 
 #ifdef ONLINE_JUDGE
+    #define PNAV(x)
     #define debug(x1)
     #define debug2(x1, x2)
     #define debug3(x1, x2, x3)
     #define debug4(x1, x2, x3, x4)
     #define debug5(x1, x2, x3, x4, x5)
     #define debug6(x1, x2, x3, x4, x5, x6)
-#define endl '\n'
+    #define debug7(x1, x2, x3, x4, x5, x6, x7)
+    #define debug8(x1, x2, x3, x4, x5, x6, x7, x8)
+    #define debug9(x1, x2, x3, x4, x5, x6, x7, x8, x9)
+    #define debug10(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10)
+    #define debugab(x1,s,e)
+    #define endl '\n'
 #endif
-void _debug_p(int T);
-void _debug_p(int32_t T);
-void _debug_p(unsigned int T);
-void _debug_p(unsigned T);
-void _debug_p(double T);
-void _debug_p(string T);
-void _debug_p(const char T[]);
-void _debug_p(char T);
-void _debug_p(bool T);
-template <typename T> void _debug_p(const vector<T> &vec);
-template <typename T> void _debug_p(const set<T> &vec);
-template <typename T> void _debug_p(const unordered_set<T> &vec);
-template <typename T> void _debug_p(const multiset<T> &vec);
-template <typename T> void _debug_p(const unordered_multiset<T> &vec);
-template <typename t1, typename t2> void _debug_p(const map<t1, t2> &mp);
-template <typename t1, typename t2> void _debug_p(const unordered_map<t1, t2> &mp);
-template <typename t1, typename t2> void _debug_p(const multimap<t1, t2> &mp);
-template <typename t1, typename t2> void _debug_p(const unordered_multimap<t1, t2> &mp);
-template <class... T> void _debug_p(const tuple<T...> &_tup);
-template <class TupType, size_t... I> void _debug_p_tup(const TupType &_tup, index_sequence<I...>);
-template <typename T> void _debug_p_vector_type(const T &t);
-template <typename T> void _debug_p_map_type(const T &t);
-template <typename t1, typename t2> void _debug_p(const pair<t1, t2> &p);
 // clang-format on
 /* TEMPLATE END */
 #pragma endregion
-int N, x, y, Q, tp;
-const int NMAX = 2e5 + 10;
-array<int, NMAX> prefix;
-array<int, NMAX> suffix;
-const int EMPTY_ANS = LLONG_MAX;
-int fn(int a, int b)
-{
-    return min(a, b);
-}
+int N, x, y, Q;
+const int NMAX = 2e5 + 5;
+array<int, NMAX> arr;
+array<int, NMAX> upper;
+array<int, NMAX> lower;
+
 struct SegmentTree
 {
     int val;
-    int l, r;
-    SegmentTree *lower = NULL;
-    SegmentTree *upper = NULL;
-    SegmentTree *parent = NULL;
-    bool overlap(int _l, int _r)
+    int index_l;
+    int index_r;
+    shared_ptr<SegmentTree> lower;
+    shared_ptr<SegmentTree> upper;
+    bool is_leaf = false;
+
+    SegmentTree(const int _index_l,
+                const int _index_r)
     {
-        return max(r, _r) - min(l, _l) <= (r - l) + (_r - _l);
+        index_l = _index_l;
+        index_r = _index_r;
+        is_leaf = (index_l == index_r);
     }
-    bool enclosed_in(int _l, int _r)
+    bool overlap(const int &other_index_l, const int &other_index_r) const
     {
-        return _l <= l && r <= _r;
+        return (index_l <= other_index_r) &&
+               (index_r >= other_index_l);
+    }
+    bool enclosed_in(const int &other_index_l, const int &other_index_r) const
+    {
+        return (other_index_l <= index_l) &&
+               (index_r <= other_index_r);
+    }
+    bool index_lies_in_node(const int &index) const
+    {
+        return (index_l <= index) &&
+               (index <= index_r);
+    }
+    static void update_segment_tree(shared_ptr<SegmentTree> &root, const int &update_node_index, const int &update_node_new_val)
+    {
+        if (!root)
+            return;
+        root->update_node(update_node_index, update_node_new_val);
+        return;
+    }
+    static pair<bool, int> query_segment_tree(shared_ptr<SegmentTree> &root, const int &target_l, const int &target_r)
+    {
+        return root->query_tree(target_l, target_r);
+    }
+    bool update_node(const int &update_node_index, const int &update_node_new_val)
+    {
+        if (!index_lies_in_node(update_node_index))
+            return false;
+        if (is_leaf)
+        {
+            val = update_node_new_val;
+            return true;
+        }
+        if (lower->update_node(update_node_index, update_node_new_val) ||
+            upper->update_node(update_node_index, update_node_new_val))
+        {
+            this->update_node_based_on_children();
+            return true;
+        }
+        return false;
+    }
+
+    const int INVALID_ANSWER = 0; //return in place of invalid ans
+    template <typename type_t>
+    static shared_ptr<SegmentTree> build_segment_tree(const type_t &arr,
+                                                      const int &curr_index_l,
+                                                      const int &curr_index_r)
+    {
+        shared_ptr<SegmentTree> node = make_shared<SegmentTree>(curr_index_l, curr_index_r);
+        if (node->is_leaf)
+        {
+            node->val = arr[curr_index_l];
+        }
+        else
+        {
+            int curr_index_m = curr_index_l + (curr_index_r - curr_index_l) / 2;
+            node->lower = build_segment_tree(arr, curr_index_l, curr_index_m);
+            node->upper = build_segment_tree(arr, curr_index_m + 1, curr_index_r);
+            node->update_node_based_on_children();
+        }
+        return node;
+    }
+    void update_node_based_on_children()
+    {
+        if (is_leaf)
+            return;
+        this->val = min(lower->val, upper->val);
+    }
+    pair<bool, int> query_tree(const int &target_l, const int &target_r) const
+    {
+        if (enclosed_in(target_l, target_r))
+            return {true, val};
+        else if (overlap(target_l, target_r) && !is_leaf)
+        {
+            const auto &[lower_valid, lower_ans] = lower->query_tree(target_l, target_r);
+            const auto &[upper_valid, upper_ans] = upper->query_tree(target_l, target_r);
+            if (!lower_valid)
+                return {upper_valid, upper_ans};
+            if (!upper_valid)
+                return {lower_valid, lower_ans};
+            //Merge the answers together
+            return {true, min(lower_ans, upper_ans)};
+        }
+        return {false, INVALID_ANSWER};
     }
 };
-array<SegmentTree *, NMAX> leafs_suffix;
-array<SegmentTree *, NMAX> leafs_prefix;
-SegmentTree *build_segment_tree(array<int, NMAX> &arr, int st, int ed, array<SegmentTree *, NMAX> &leafs, SegmentTree *par = NULL)
-{
-    if (st > ed)
-        return NULL;
-    SegmentTree *node = new SegmentTree();
-    node->parent = par;
-    node->l = st;
-    node->r = ed;
-    if (st == ed)
-    {
-        node->val = arr[st];
-        leafs[st] = node;
-    }
-    else
-    {
-        int m = st + (ed - st) / 2;
-        node->lower = build_segment_tree(arr, st, m, leafs, node);
-        node->upper = build_segment_tree(arr, m + 1, ed, leafs, node);
-        node->val = fn(node->lower->val, node->upper->val);
-    }
-    return node;
-}
-int query_tree(SegmentTree *node, int l, int r)
-{
-    if (node == NULL)
-        return EMPTY_ANS;
 
-    if (node->enclosed_in(l, r))
-        return node->val;
-    else if (node->overlap(l, r))
-        return fn(query_tree(node->lower, l, r), query_tree(node->upper, l, r));
-    else
-        return EMPTY_ANS;
-}
-void update_node(SegmentTree *node)
-{
-    if (node == NULL)
-        return;
-    node->val = fn(node->lower->val, node->upper->val);
-    update_node(node->parent);
-}
-void update_node(SegmentTree *node, int new_val)
-{
-    node->val = new_val;
-    update_node(node->parent);
-}
 void solve()
 {
     cin >> N >> Q;
-    forab(i, 1, N + 1)
+    forab(index, 1, N + 1)
     {
-        cin >> prefix[i];
-        suffix[i] = prefix[i];
-        suffix[i] += i;
-        prefix[i] += (N - (i - 1));
+        cin >> arr[index];
+        lower[index] = arr[index] + (N - index + 1);
+        upper[index] = arr[index] + (index);
     }
-#ifndef ONLINE_JUDGE
-    forab(i, 1, N + 1)
+
     {
-        cout << prefix[i] << " ";
-    }
-    cout << endl;
-    forab(i, 1, N + 1)
-    {
-        cout << suffix[i] << " ";
-    }
-    cout << endl;
-#endif
-    SegmentTree *suffix_tree = build_segment_tree(suffix, 1, N, leafs_suffix);
-    SegmentTree *prefix_tree = build_segment_tree(prefix, 1, N, leafs_prefix);
-    forab(i, 0, Q)
-    {
-        cin >> tp;
-        if (tp == 1)
+        int tp, price, index;
+        shared_ptr<SegmentTree> lower_tree = SegmentTree::build_segment_tree(lower, 1, N);
+        shared_ptr<SegmentTree> upper_tree = SegmentTree::build_segment_tree(upper, 1, N);
+        forab(i, 0, Q)
         {
-            cin >> x >> y;
-            int prefix_val = y + N - (x - 1);
-            update_node(leafs_prefix[x], prefix_val);
-            int suffix_val = y + x;
-            update_node(leafs_suffix[x], suffix_val);
-        }
-        else if (tp == 2)
-        {
-            cin >> x;
-            int prefix_ans = query_tree(prefix_tree, 1, x) - (N - (x - 1));
-            int suffix_ans = query_tree(suffix_tree, x, N) - x;
-            debug2(prefix_ans, suffix_ans);
-            cout << min(prefix_ans, suffix_ans) << endl;
+            cin >> tp;
+            if (tp == 1)
+            {
+                cin >> index >> price;
+                arr[index] = price;
+                SegmentTree::update_segment_tree(lower_tree, index, arr[index] + (N - index + 1));
+                SegmentTree::update_segment_tree(upper_tree, index, arr[index] + (index));
+            }
+            else if (tp == 2)
+            {
+                cin >> index;
+                auto const &[lower_valid, lower_ans] = SegmentTree::query_segment_tree(lower_tree, 1, index);
+                auto const &[upper_valid, upper_ans] = SegmentTree::query_segment_tree(upper_tree, index, N);
+                cout << min({lower_ans - (N - index + 1), upper_ans - (index)}) << endl;
+            }
         }
     }
 }
@@ -192,9 +237,10 @@ int32_t main()
     cout.tie(0);
 #endif
 #ifdef _DEBUG_CODE
+    cout << "DEBUGGING ON" << endl;
+    cout.flush();
     freopen("/home/yaduraj/CC/IN.txt", "r", stdin);
     freopen("/home/yaduraj/CC/OUT.txt", "w", stdout);
-    cout << "DEBUGGING" << endl;
 #endif
     // int T;
     // cin >> T;
@@ -204,6 +250,7 @@ int32_t main()
 }
 #pragma region
 // clang-format off
+#ifndef ONLINE_JUDGE
 void _debug_p(int T) { cout << T; }
 void _debug_p(int32_t T) { cout << T; }
 void _debug_p(unsigned int T) { cout << T; }
@@ -214,6 +261,8 @@ void _debug_p(const char T[]) { cout << '"' << T << '"'; }
 void _debug_p(char T) { cout << '\'' << T << '\''; }
 void _debug_p(bool T) { cout << (T ? '1' : '0') << 'b'; }
 template <typename T> void _debug_p(const vector<T> &vec) { _debug_p_vector_type(vec); }
+template <typename T> void _debug_p(const list<T> &vec) { _debug_p_vector_type(vec); }
+template <typename T,size_t sz> void _debug_p(const array<T,sz> &vec) { _debug_p_vector_type(vec); }
 template <typename T> void _debug_p(const set<T> &vec) { _debug_p_vector_type(vec); }
 template <typename T> void _debug_p(const unordered_set<T> &vec) { _debug_p_vector_type(vec); }
 template <typename T> void _debug_p(const multiset<T> &vec) { _debug_p_vector_type(vec); }
@@ -240,17 +289,29 @@ template <typename T> void _debug_p_vector_type(const T &t) {
     }
     cout << ']';
 }
+template <typename T> void _debug_p_vector_typeab(const T &t,int s,int e) { 
+    cout << '[';
+    bool first = true;
+    forab (i,s,e)
+    {
+        if (!first)
+            cout << ',' << ' ';
+        _debug_p(t[i]);
+        first = false;
+    }
+    cout << ']';
+}
 template <typename T> void _debug_p_map_type(const T &t) { 
     cout << '[';
     bool first = true;
-    for (const auto &[it, it2] : t)
+    for (const auto &[key, val] : t)
     {
         if (!first)
             cout << ',' << ' ';
         cout << '{';
-        _debug_p(it);
+        _debug_p(key);
         cout << ':';
-        _debug_p(it2);
+        _debug_p(val);
         cout << '}';
         first = false;
     }
@@ -263,4 +324,5 @@ template <typename t1, typename t2> void _debug_p(const pair<t1, t2> &p) {
     _debug_p(p.second);
     cout << ')';
 }
+#endif
 #pragma endregion
